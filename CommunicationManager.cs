79203c253e23500387a48ea150eb685893690148
Igor Radkevich <landrommigor@gmail.com>
2023-03-01 20:53:52 +0300

    Добавьте файлы проекта.

---
+++ CommunicationManager.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SNT
+{
+    internal class CommunicationManager
+    {
+        public enum TransmissionType { Text, Hex }
+
+        //property variables
+        private string _baudRate = string.Empty;
+        private string _parity = string.Empty;
+        private string _stopBits = string.Empty;
+        private string _dataBits = string.Empty;
+        private string _portName = string.Empty;
+        private int count;
+        private List<string> _dataByteList;
+        private TransmissionType _transType;
+
+
+        #region Manager Properties
+        public List<string> DataByteList
+        {
+            get { return _dataByteList; }
+            set { _dataByteList = value; }
+        }
+
+        public int Count
+        {
+            get { return count; }
+            set { count = value; }
+        }
+
+        /// <summary>
+        /// Property to hold the BaudRate
+        /// of our manager class
+        /// </summary>
+        public string BaudRate
+        {
+            get { return _baudRate; }
+            set { _baudRate = value; }
+        }
+
+        /// <summary>
+        /// property to hold the Parity
+        /// of our manager class
+        /// </summary>
+        public string Parity
+        {
+            get { return _parity; }
+            set { _parity = value; }
+        }
+
+        /// <summary>
+        /// property to hold the StopBits
+        /// of our manager class
+        /// </summary>
+        public string StopBits
+        {
+            get { return _stopBits; }
+            set { _stopBits = value; }
+        }
+
+        /// <summary>
+        /// property to hold the DataBits
+        /// of our manager class
+        /// </summary>
+        public string DataBits
+        {
+            get { return _dataBits; }
+            set { _dataBits = value; }
+        }
+
+        /// <summary>
+        /// property to hold the PortName
+        /// of our manager class
+        /// </summary>
+        public string PortName
+        {
+            get { return _portName; }
+            set { _portName = value; }
+        }
+
+        /// <summary>
+        /// property to hold our TransmissionType
+        /// of our manager class
+        /// </summary>
+        public TransmissionType CurrentTransmissionType
+        {
+            get { return _transType; }
+            set { _transType = value; }
+        }
+
+        #endregion
+
+        private SerialPort comPort = new SerialPort();
+
+
+        public CommunicationManager(string baud, string par, string sBits, string dBits, string name)
+        {
+            _baudRate = baud;
+            _parity = par;
+            _stopBits = sBits;
+            _dataBits = dBits;
+            _portName = name;
+            _dataByteList= new List<string>();
+
+            //now add an event handler
+            comPort.DataReceived += new SerialDataReceivedEventHandler(comPort_DataReceived);
+        }
+        public CommunicationManager()
+        {
+            _baudRate = string.Empty;
+            _parity = string.Empty;
+            _stopBits = string.Empty;
+            _dataBits = string.Empty;
+            _portName = "COM2";
+            _dataByteList = new List<string>();
+
+            //add event handler
+            comPort.DataReceived += new SerialDataReceivedEventHandler(comPort_DataReceived);
+        }
+
+        /// <summary>
+        /// Метод проверки открытия COM-порта.
+        /// </summary>
+        public bool ComPortIsOpen()
+        {
+            if (comPort.IsOpen && !comPort.BreakState)
+                return true;
+            else
+                return false;
+        }
+
+        #region OpenPort
+        /// <summary>
+        /// Метод открытия COM-порта.
+        /// </summary>
+        public bool OpenPort()
+        {
+            try
+            {
+                if (comPort.IsOpen) comPort.Close();
+                comPort.BaudRate = int.Parse(_baudRate);    //BaudRate
+                comPort.DataBits = int.Parse(_dataBits);    //DataBits
+                comPort.StopBits = (StopBits)Enum.Parse(typeof(StopBits), _stopBits);    //StopBits
+                comPort.Parity = (Parity)Enum.Parse(typeof(Parity), _parity);    //Parity
+                comPort.PortName = _portName;   //PortName
+                Console.WriteLine("Read_TimeOut = " + comPort.ReadTimeout);
+                //comPort.ReadTimeout = 1000;
+                comPort.Open();
+
+                Console.WriteLine(("Открытие COM-порта: " + comPort.PortName));
+                Console.WriteLine("COM-порт открыт в |" + DateTime.Now + "|\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+                return false;
+            }
+        }
+        #endregion
+
+        #region ClosePort
+        /// <summary>
+        /// Метод закрытия COM-порта.
+        /// </summary>
+        public bool ClosePort()
+        {
+            try
+            {
+                if (comPort.IsOpen == true || comPort.BreakState)
+                {
+                    comPort.BreakState = false;
+                    comPort.Close();
+                }
+                Console.WriteLine(("Закрытие COM-порта: " + comPort.PortName));
+                Console.WriteLine("COM-порт закрыт в |" + DateTime.Now + "|\n");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message + "\n");
+                return false;
+            }
+        }
+        #endregion
+
+        #region HexToByte
+        /// <summary>
+        /// method to convert hex string into a byte array
+        /// </summary>
+        /// <param name="msg">string to convert</param>
+        /// <returns>a byte array</returns>
+        private byte[] HexToByte(string msg)
+        {
+            msg = msg.Replace(" ", "");
+            byte[] comBuffer = new byte[msg.Length / 2];
+            for (int i = 0; i < msg.Length; i += 2)
+                comBuffer[i / 2] = (byte)Convert.ToByte(msg.Substring(i, 2), 16);
+            return comBuffer;
+        }
+        #endregion
+
+        #region ByteToHex
+        /// <summary>
+        /// method to convert a byte array into a hex string
+        /// </summary>
+        /// <param name="comByte">byte array to convert</param>
+        /// <returns>a hex string</returns>
+        private string ByteToHex(byte[] comByte)
+        {
+            //create a new StringBuilder object
+            StringBuilder builder = new StringBuilder(comByte.Length * 3);
+            //loop through each byte in the array
+            foreach (byte data in comByte)
+                //convert the byte to a string and add to the stringbuilder
+                builder.Append(Convert.ToString(data, 16).PadLeft(2, '0'));
+            //return the converted value
+            return builder.ToString().ToUpper();
+        }
+        #endregion
+
+
+        #region comPort_DataReceived
+        /// <summary>
+        /// Метод, который будет вызываться, когда в буфере ожидают данные.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void comPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            if (!comPort.BreakState)
+            {
+                try
+                {
+                    int bytes = comPort.BytesToRead;
+                    byte[] comBuffer = new byte[bytes];
+                    comPort.Read(comBuffer, 0, bytes);
+                    Console.WriteLine("{0} " + ByteToHex(comBuffer), count);
+                    count += bytes;
+                    foreach (var item in comBuffer)
+                    {
+                        _dataByteList.Add(Convert.ToString(item, 16).PadLeft(2, '0').ToUpper());
+
+                    }
+                    Console.WriteLine("Размер коллекции: " + _dataByteList.Count);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Ошибка чтения с порта." + ex.Message);
+                }
+            }
+            else
+            {
+                string str = comPort.ReadExisting();
+            }
+
+        }
+        #endregion
+
+        #region WriteData
+        /// <summary>
+        /// Метод отправки данных-hex счетчику.
+        /// </summary>
+        /// <param name="msg">string hex</param>
+        public void WriteData(string msg)
+        {
+            switch (CurrentTransmissionType)
+            {
+                case TransmissionType.Text:
+                    if (!(comPort.IsOpen == true))
+                        comPort.Open();
+                    comPort.WriteLine(msg);
+                    Console.WriteLine(msg + "\n");
+                    break;
+                case TransmissionType.Hex:
+                    try
+                    {
+                        if (!(comPort.IsOpen == true))
+                            comPort.Open();
+                        byte[] newMsg = HexToByte(msg);
+                        Console.WriteLine("\n\n" + "ОТПРАВКА hex-сообщения счетчику: |" + msg);
+                        comPort.Write(newMsg, 0, newMsg.Length);
+                    }
+                    catch (FormatException ex)
+                    {
+                        Console.WriteLine("Ошибка отправки hex - сообщения счетчику:" + ex.Message);
+                        byte[] newMsg = HexToByte(msg);
+                        Console.WriteLine(ByteToHex(newMsg) + "\n");
+                    }
+                    break;
+                default:
+                    //first make sure the port is open
+                    //if its not open then open it
+                    if (!(comPort.IsOpen == true))
+                        comPort.Open();
+                    //send the message to the port
+                    comPort.Write(msg);
+                    //display the message
+                    //DisplayData_Rch(msg + "\n");
+                    break;
+            }
+        }
+        #endregion
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace SNT
+{
+    internal class Program
+    {
+        static int temp_Timeout = 500;
+        //--------------------------
+        static string temp_PortName;
+        static string temp_BaudRate;
+        static string temp_Parity;
+        static string temp_StopBits;
+        static string temp_DataBits;
+        readonly static string check_hex = "AAAAAA0F6210D8";
+        readonly static string RTC_write_hex = "AAAAAA0362100000D4";
+        readonly static string RTC_read_hex = "AAAAAA046210D3";
+
+        static CommunicationManager comm = new CommunicationManager();
+
+
+        static void Main(string[] args)
+        {
+
+            ParamFromConfiguration_Load();
+            OpenComPort();
+
+            SendSingleDataCOM(check_hex);
+            Wait(250);
+
+            SendSingleDataCOM(RTC_write_hex);
+            Wait(250);
+
+            comm.Count = 0;
+            comm.DataByteList.Clear();
+
+            SendSingleDataCOM(RTC_read_hex);
+            Wait(500);
+            Console.WriteLine("-----------------------STOP----------------------");
+            foreach (var item in comm.DataByteList)
+            {
+                Console.WriteLine(item);
+            }
+            comm.ClosePort();
+            
+            Console.ReadLine();
+
+        }
+
+        #region Wait TimeOut
+        // Методы реализации задержки отображения без блокировки потока пользовательского интерфейса.
+        public static void Wait(int interval)
+        {
+            ExecuteWait(() => Thread.Sleep(interval));
+        }
+        public static void ExecuteWait(Action action)
+        {
+            var waitFrame = new DispatcherFrame();
+            // Use callback to "pop" dispatcher frame
+            IAsyncResult op = action.BeginInvoke(dummy => waitFrame.Continue = false, null);
+            // this method will block here but window messages are pumped
+            Dispatcher.PushFrame(waitFrame);
+            // this method may throw if the action threw. caller responsibility to handle.
+            action.EndInvoke(op);
+        }
+        #endregion
+
+        static void ParamFromConfiguration_Load()
+        {
+            try
+            {
+                IniFile INI = new IniFile(@ConfigurationManager.AppSettings["pathConfig"]);
+                temp_PortName = INI.ReadINI("COMportSettings", "PortName");
+                temp_BaudRate = INI.ReadINI("COMportSettings", "BaudRate");
+                temp_Parity = INI.ReadINI("COMportSettings", "Parity");
+                temp_StopBits = INI.ReadINI("COMportSettings", "StopBits");
+                temp_DataBits = INI.ReadINI("COMportSettings", "DataBits");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка чтения config.ini файла!\n" + ex,
+                                "Ошибка !");
+            }
+        }
+
+        // Метод открытия COM-порта.
+        static public void OpenComPort()
+        {
+            comm.Parity = temp_Parity;
+            comm.StopBits = temp_StopBits;
+            comm.DataBits = temp_DataBits;
+            comm.BaudRate = temp_BaudRate;
+            comm.PortName = temp_PortName;
+            comm.CurrentTransmissionType = CommunicationManager.TransmissionType.Hex;
+            comm.OpenPort();
+            if (comm.ComPortIsOpen())
+            {
+
+                Console.WriteLine("Параметры COM-порта:\n" +
+                     "Четность: " + temp_Parity + "\n" +
+                     "Стоповые биты: " + temp_StopBits + "\n" +
+                     "Биты данных: " + temp_DataBits + "\n" +
+                     "Бит в секунду: " + temp_BaudRate + "\n" +
+                     "Таймаут: " + temp_Timeout + "\n");
+            }
+        }
+
+        // Метод отправки запроса и чтение параметра, отображаеммого элемента данных.
+        static void SendSingleDataCOM(string tempHex)
+        {
+            comm.WriteData(tempHex);
+        }
+    }
+}
